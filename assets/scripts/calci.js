c909911ba900ef13948f0f71da77adf836ce1c35
VenkatramanC <venkatraman.c9@gmail.com>
2015-09-05 12:14:35 +0530

    origin master

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,1 +1,23 @@
-calci.js
+$(document).ready(function(){
+  $('#keys .key').click(function(){
+  	var input = 
+  	if(this.dataset.keytype == "digit" ){
+    input = this.dataset.digit
+
+  	}(this.dataset.keytype == "opeartor"){
+  		input = this.dataset.operator;
+  	}
+
+    $('#preview').html($("#preview").html() + input);
+  });
+
+  $('#operators .operator').click(function(){
+    $('#preview').html($("#preview").html() + this.dataset.digit);
+  });
+  
+  $("#delete").click(function() {
+    $('#preview').html($("#preview").html().slice(0 , -1));
+  
+ 
+  });
+});
