ae1a158d18fccf9230b83e185215c3829d6e1588
VenkatramanC <venkatraman.c9@gmail.com>
2015-09-05 13:01:37 +0530

    Creating the Layout

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,23 +1,20 @@
 $(document).ready(function(){
-  $('#keys .key').click(function(){
-  	var input = 
-  	if(this.dataset.keytype == "digit" ){
-    input = this.dataset.digit
-
-  	}(this.dataset.keytype == "opeartor"){
+  $('calculator .input').click(function(){
+  	var input;
+  	if(this.dataset.keyType == "digit" ){
+    input = this.dataset.digit;
+    }
+      else if(this.dataset.keyType == "operator"){
   		input = this.dataset.operator;
   	}
-
+       else if(this.dataset.keyType == "delete"){
+       	 $('#preview').html($("#preview").html().slice(0 , -1));
+       }
+       else if(this.dataset.keyType == "equals"){
+        $('#result').html(eval($('#preview').html()));
+       }
+    if(typeof(input) != 'undefined'){
     $('#preview').html($("#preview").html() + input);
-  });
-
-  $('#operators .operator').click(function(){
-    $('#preview').html($("#preview").html() + this.dataset.digit);
-  });
-  
-  $("#delete").click(function() {
-    $('#preview').html($("#preview").html().slice(0 , -1));
-  
- 
-  });
+  }
+    });
 });
