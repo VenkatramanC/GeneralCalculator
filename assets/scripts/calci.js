f466ba1e921f5778f67fb67d3a66e8bb4a3bf8f9
VenkatramanC <venkatraman.c9@gmail.com>
2015-09-06 12:57:15 +0530

    Hitting backspace delete the char

--- assets/scripts/calci.js
+++ assets/scripts/calci.js
@@ -1,20 +1,45 @@
-$(document).ready(function(){
-  $('calculator .input').click(function(){
-  	var input;
-  	if(this.dataset.keyType == "digit" ){
-    input = this.dataset.digit;
+var calculator = {
+	init: function() {
+     $('#calculator .input').click(function(){
+  	   if(this.dataset.keyType == "digit" ){
+         calculator.handleInput(this.dataset.digit);
     }
-      else if(this.dataset.keyType == "operator"){
-  		input = this.dataset.operator;
+       else if(this.dataset.keyType == "operator"){
+  	     calculator.handleInput(this.dataset.operator);
   	}
        else if(this.dataset.keyType == "delete"){
-       	 $('#preview').html($("#preview").html().slice(0 , -1));
-       }
+         calculator.handleDelete();	 
+    }
        else if(this.dataset.keyType == "equals"){
-        $('#result').html(eval($('#preview').html()));
-       }
-    if(typeof(input) != 'undefined'){
-    $('#preview').html($("#preview").html() + input);
-  }
+          calculator.evalualteResult();
+    }
+   });
+      $('#calculator.delete').dbclick(function() {
+  	   $('#preview').html('');
+  	   $('#result').html('')
+  });
+
+
+     ['0','1','2','3','4','5','6','7','8','9'].forEach(function(digit){
+     $(document).bind('keyup' , 'digit',function(){
+       calculator.handleInput(digit);
+     });
+    }); 
+    $(document).bind ('keyup', 'backspace' ,function() {
+      calculator.handleDelete();	 
     });
-});
+    
+	},
+    handleInput: function(input) {
+     $('#preview').html($("#preview").html() + input);
+    },  
+    handleDelete: function() {
+	 $('#preview').html($("#preview").html().slice(0 , -1));
+    },
+    evalualteResult: function(){
+      $('#result').html(eval($('#preview').html()));
+    }
+}
+  $(document).ready(function(){
+   calculator.init();
+  });
